d53232125d53fe181748e440e00fe88201513b0c
ZaymerOne <77398037+ZaymerOne@users.noreply.github.com>
2021-11-19 20:46:16 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,3 +1,5 @@
+import math
+
 a = float(input("Введите число a:" ))
 b = float(input("Введите число b:"))
 c = a + b
@@ -16,4 +18,11 @@ c = a // b
 print("a // b = ",c)
 
 d = input("Введите свое имя: ")
-print("Hello "+ d + " !")
+print("Hello, "+ d + " !")
+
+s = list(input("Введите слово: "))
+print("Результат среза: ", s[-1:-3:-1])
+
+print("Ищем площадь окружности S")
+r = float(input("Введите радиус окружности r: "))
+print("S = pi * r**2 = ", math.pi * (r ** 2))
