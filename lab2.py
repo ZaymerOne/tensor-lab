0ee20363fb19fc6493468484aff775f6f86be1f5
ZaymerOne <77398037+ZaymerOne@users.noreply.github.com>
2021-11-19 20:16:39 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,19 @@
+a = float(input("Введите число a:" ))
+b = float(input("Введите число b:"))
+c = a + b
+print("a + b = ",c)
+c = a - b
+print("a - b = ",c)
+c = a * b
+print("a * b = ",c)
+c = a / b
+print("a / b = ",c)
+c = a ** b
+print("a ** b = ",c)
+c = a % b
+print("a % b = ",c)
+c = a // b
+print("a // b = ",c)
+
+d = input("Введите свое имя: ")
+print("Hello "+ d + " !")
